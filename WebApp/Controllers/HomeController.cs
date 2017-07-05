79a7dff737bfcca06ad7bea4689c75fe0d5dc19c
nikos damis <nikosdamis@hotmail.com>
2017-07-05 23:31:51 +0300

    Add project files.

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Net.Http.Headers;
+using System.IO;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IHostingEnvironment hostingEnv;
+
+        public HomeController(IHostingEnvironment env)
+        {
+            this.hostingEnv = env;
+        }
+
+        [HttpPost]
+        public IActionResult UploadFile(IFormFile file)
+        {
+            long size = 0;
+
+            var filename = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+            filename = hostingEnv.WebRootPath + "\\" + (string)filename;
+            size += file.Length;
+            using (FileStream fs = System.IO.File.Create(filename))
+            {
+                file.CopyTo(fs);
+                fs.Flush();
+            }
+
+            ViewBag.Message = $"File uploaded into {filename}, {size} bytes uploaded successfully";
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
